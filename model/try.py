a9e3626ce0ad2b781d2ae1cdd0e8faf8a3cc30dc
TaoZI <724683889@qq.com>
2017-01-06 10:03:18 +0800

    Add files via upload

---
+++ model/try.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+import numpy as np
+# df1 = pd.DataFrame([[2,3,4,5],[3,2,4,1],[7,8,5,4]],columns = ['A','B','C','D'])
+
+import itertools
+for com in itertools.combinations([1,2,3,4],3):
+    print com
+
+
